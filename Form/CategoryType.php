31b0cf4c2807133074cb1d2a8aaa805e7ae441b8
Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr>
2013-01-21 14:18:57 +0100

    Add comments (licence & authors) in classes Translate the menu

--- Form/CategoryType.php
+++ Form/CategoryType.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ *
+ * @author:  Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr> 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @licence: GPL
+ *
+ */
+
 namespace IDCI\Bundle\PartnerBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
--- IDCIPartnerBundle.php
+++ IDCIPartnerBundle.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ *
+ * @author:  Baptiste BOUCHEREAU <baptiste.bouchereau@idci-consulting.fr> 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @licence: GPL
+ *
+ */
+
 namespace IDCI\Bundle\PartnerBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
