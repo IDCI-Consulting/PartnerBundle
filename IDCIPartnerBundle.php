ee607495bb8009378906d0889f88dda49649d9d5
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2013-05-18 23:41:52 +0200

    Fix wrong use !

--- IDCIPartnerBundle.php
+++ IDCIPartnerBundle.php
@@ -10,10 +10,7 @@
 
 namespace IDCI\Bundle\PartnerBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use IDCI\Bundle\ExporterBundle\DependencyInjection\IDCIExporterExtension;
 
 class IDCIPartnerBundle extends Bundle
 {
